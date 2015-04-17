d737fea38b33a51d9ec4c540ef74eca2a8f10157
lucyanne <lucyannerichards@gmail.com>
2015-04-16 20:25:01 -0700

    yay test data working

--- data/curriculum.js
+++ data/curriculum.js
@@ -1,1 +1,1 @@
-CURRICULUM = [{ "title" : "Vascular Surgery", "lessons" : [ "1000002", "1000015", "1000099", "1000142", "1000180", "1000247", "1000255", "1000270" ], "condition" : "1", "nh_id" : "100001", "_id" : "EH5Jv5d6XNaYQepuK" }];
+CURRICULUM = [{ "title" : "Vascular Surgery", "lessons" : [ "1000642", "1000645", "1000614", "1000615", "1000618", "1000620", "1000647", "1000619" ], "condition" : "1", "nh_id" : "100001", "_id" : "EH5Jv5d6XNaYQepuK" }];
